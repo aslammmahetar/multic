8e3b45c44ed230b64231d03510bc133d9d5386f2
Aslam M Mahetar <115460260+aslammmahetar@users.noreply.github.com>
2026-04-24 11:21:37 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const socket = io(https://mt-backend-ni50.onrender.com, {
+const socket = io("https://mt-backend-ni50.onrender.com", {
     withCredentials: true,
     autoConnect: true
 });
